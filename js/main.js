5f27404996c8f48108f4ff53c8e8ccdb6c256f42
Ugo <ugo.briasco@sumup.com>
2018-03-10 23:19:31 +0100

    Review accessibility tree
    Include aria attributes for links

--- js/main.js
+++ js/main.js
@@ -158,6 +158,7 @@ createRestaurantHTML = restaurant => {
 
   const more = document.createElement("a");
   more.innerHTML = "View Details";
+  more.setAttribute("aria-label", "More details about" + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
 
   const li = document.createElement("li");
