eb91f10b59596241a5a2922cb9a1aa3ec69acb89
Ugo <ugo.briasco@sumup.com>
2018-03-18 21:17:31 +0100

    Resize static images by 75%

--- sw.js
+++ sw.js
@@ -13,16 +13,16 @@ self.addEventListener("install", event => {
         "./js/dbhelper.js",
         "./js/main.js",
         "./js/restaurant_info.js",
-        "./img/1.jpg",
-        "./img/2.jpg",
-        "./img/3.jpg",
-        "./img/4.jpg",
-        "./img/5.jpg",
-        "./img/6.jpg",
-        "./img/7.jpg",
-        "./img/8.jpg",
-        "./img/9.jpg",
-        "./img/10.jpg"
+        "./img/w600_1.jpg",
+        "./img/w600_2.jpg",
+        "./img/w600_3.jpg",
+        "./img/w600_4.jpg",
+        "./img/w600_5.jpg",
+        "./img/w600_6.jpg",
+        "./img/w600_7.jpg",
+        "./img/w600_8.jpg",
+        "./img/w600_9.jpg",
+        "./img/w600_10.jpg"
       ]);
     })
   );
